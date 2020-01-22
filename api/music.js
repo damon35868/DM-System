ba17c09826fc1649296250e27ad3d5b12107ad5e
D.M <578328962@qq.com>
2020-01-22 14:44:25 +0800

    随机显示23个排行榜单

--- api/music.js
+++ api/music.js
@@ -4,7 +4,9 @@ module.exports = app => {
 	const router = new express.Router();
 	
 	 router.get('/musiclist',(req,res)=>{
-		 musicApi.netease.getTopList( 0 ).then(data=>{
+		 //随机显示23个排行榜单
+		 const randomNum = Math.round(Math.random()*23);
+		 musicApi.netease.getTopList( randomNum ).then(data=>{
 		 	res.status(200).json({msg:'获取成功',data});
 		 }).catch(erro=>console.log(erro));
 		 
