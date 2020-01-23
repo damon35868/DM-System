d818894f363ee58115614a82dd6b92de960b76ca
D.M <578328962@qq.com>
2020-01-23 15:51:44 +0800

    添加qq音乐榜单

--- api/music.js
+++ api/music.js
@@ -6,14 +6,24 @@ module.exports = app => {
 
 	//top榜单
 	router.get('/musiclist', (req, res) => {
-		//随机显示23个排行榜单
-		const randomNum = Math.round(Math.random() * 23);
-		musicApi.netease.getTopList(randomNum).then(data => {
+		//随机显示qq和网易供应商资料
+		const randomVendor = Math.round(Math.random() * 1);
+		//随机显示网易23个排行榜单
+		const neteaseRandomNum = Math.round(Math.random() * 23);
+		//随机显示qq排行榜
+		const qqRandomNum = Math.round(Math.random() * 10);
+		const qqArr = [3,5,6,16,17,18,19,20,21,22,23];
+		
+		let vendor;
+		if (randomVendor === 0) vendor = musicApi.netease;
+		else vendor = musicApi.qq;
+		vendor.getTopList(randomVendor === 0 ? neteaseRandomNum : qqArr[qqRandomNum]).then(data => {
 			res.status(200).json({
 				msg: '获取成功',
 				data
 			});
 		}).catch(erro => console.log(erro));
+
 
 	});
 
@@ -25,14 +35,14 @@ module.exports = app => {
 		if (!query.keyword) return res.status(404).json({
 			msg: '关键字不能为空'
 		});
-		if(query.type !== 'qq' && query.type !== 'xiami' && query.type !== 'netease') searchType = 'xiami';
+		if (query.type !== 'qq' && query.type !== 'xiami' && query.type !== 'netease') searchType = 'xiami';
 		else searchType = query.type;
-		
+
 		switch (searchType) {
 			case 'netease':
 				musicApi.netease.searchSong({
 					keyword: query.keyword,
-					offset:query.offset
+					offset: query.offset
 				}).then(data => {
 					res.status(200).json({
 						msg: '搜索成功',
@@ -43,7 +53,7 @@ module.exports = app => {
 			case 'xiami':
 				musicApi.xiami.searchSong({
 					keyword: query.keyword,
-					offset:query.offset
+					offset: query.offset
 				}).then(data => {
 					res.status(200).json({
 						msg: '搜索成功',
@@ -54,7 +64,7 @@ module.exports = app => {
 			case 'qq':
 				musicApi.qq.searchSong({
 					keyword: query.keyword,
-					offset:query.offset
+					offset: query.offset
 				}).then(data => {
 					res.status(200).json({
 						msg: '搜索成功',
--- client/src/components/MusicRecommend.vue
+++ client/src/components/MusicRecommend.vue
@@ -1,74 +1,72 @@
 <template>
 	<div class="musicRecommend p-10" v-if="musicTop !== null">
-		<div class="flex pb-10 items-center">
-			<img :src="musicTop.cover" class="w-32 rounded mr-8">
-			<div>
-				<h3 class="text-3xl font-bold text-green-800 pb-4">{{musicTop.name}}</h3>
-				<h4 class="text-xl font-bold text-green-900">{{musicTop.description}}</h4>
+		<div class="flex pb-10 justify-between items-baseline">
+			<div class="flex items-center" style="max-width: 80%;">
+				<img :src="musicTop.cover" class="w-32 rounded mr-8">
+				<div>
+					<h3 class="text-3xl font-bold text-green-800 pb-4">{{musicTop.name}}</h3>
+					<h4 class="text-xl font-bold text-green-900">{{musicTop.description}}</h4>
+				</div>
 			</div>
+			<p class="text-xs text-gray-600 pb-2">播放次数：{{musicTop.playCount}}</p>
 		</div>
-		
-		<el-table
-		class="px-10"
-		  :data="musicToplist"
-		  align="center"
-		  style="width: 100%">
-		  <el-table-column
-		  align="center"
-		    label="专辑封面"
-		    width="250px">
-		    <template slot-scope="scope">
-		      <img class="w-24 rounded mx-auto" :src="scope.row.album.cover"/>
-		    </template>
-		  </el-table-column>
-		  <el-table-column
-		    label="歌曲名"
-			align="center"
-		   >
-		    <template slot-scope="scope">
-		        <div slot="reference" class="name-wrapper">
-		          <el-tag size="medium" class="font-bold">{{ scope.row.name }}</el-tag>
-		        </div>
-		    </template>
-		  </el-table-column>
-		  <el-table-column label="歌手" align="center">
+
+		<el-table class="px-10" :data="musicToplist" align="center" style="width: 100%">
+			<el-table-column align="center" label="专辑封面" width="250px">
 				<template slot-scope="scope">
-				  <span style="margin-left: 10px" class="font-bold">{{ scope.row.artists[0].name }}</span>
+					<img class="w-24 rounded mx-auto" :src="scope.row.album.cover" />
 				</template>
-		  </el-table-column>
-		  <el-table-column label="音乐链接" align="center">
+			</el-table-column>
+			<el-table-column label="歌曲名" align="center">
 				<template slot-scope="scope">
-				 <a style="margin-left: 10px" target="_blank" class="text-blue-600 underline" :href="scope.row.link">{{ scope.row.link }}</a>
+					<div slot="reference" class="name-wrapper">
+						<el-tag size="medium" class="font-bold">{{ scope.row.name }}</el-tag>
+					</div>
 				</template>
-		  </el-table-column>
+			</el-table-column>
+			<el-table-column label="歌手" align="center">
+				<template slot-scope="scope">
+					<span style="margin-left: 10px" class="font-bold">{{ scope.row.artists[0].name }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="音乐咨询供应商" align="center">
+				<template slot-scope="scope">
+					<span style="margin-left: 10px" class="font-bold">{{ scope.row.vendor }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="音乐链接" align="center">
+				<template slot-scope="scope">
+					<a style="margin-left: 10px" target="_blank" class="text-blue-600 underline" :href="scope.row.link">{{ scope.row.link }}</a>
+				</template>
+			</el-table-column>
 		</el-table>
 	</div>
 </template>
 
 <script>
 	export default {
-		name:'musicRecommend',
-		data(){
+		name: 'musicRecommend',
+		data() {
 			return {
-				musicTop:null,
-				musicToplist:[]
+				musicTop: null,
+				musicToplist: []
 			}
 		},
-		created(){
+		created() {
 			this.getMusicList();
 		},
-		methods:{
-			getMusicList(){
-				this.$http.get('/api/music/musiclist').then(res=>{
+		methods: {
+			getMusicList() {
+				this.$http.get('/api/music/musiclist').then(res => {
 					this.musicTop = res.data.data.data;
 					this.musicToplist = res.data.data.data.list;
-				}).catch(erro=>console.log(erro));
+				}).catch(erro => console.log(erro));
 			},
 			handleEdit(index, row) {
-			  console.log(index, row);
+				console.log(index, row);
 			},
 			handleDelete(index, row) {
-			  console.log(index, row);
+				console.log(index, row);
 			}
 		}
 	}
